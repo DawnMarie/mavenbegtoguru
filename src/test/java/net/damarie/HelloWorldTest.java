cd55954dcf9d7756509cb154c9b47494f5803e61
Dawn-Marie Dunn <dawn-marie.dunn@endurance.com>
2023-03-16 16:12:39 -0400

    Maven Surefire Plugin

---
+++ src/test/java/net/damarie/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package net.damarie;
+
+public class HelloWorldTest {
+
+    public void testHello() {
+        System.out.println("TEST - Hello....");
+    }
+
+    public void testWorld() {
+        System.out.println("TEST - World!!");
+    }
+
+}
