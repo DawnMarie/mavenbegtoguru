755b5e70d781da9a788560904439c43a0413720d
Dawn-Marie Dunn <dawn-marie.dunn@endurance.com>
2023-03-21 16:46:06 -0400

    Using Maven Surefire for JUnit 4 and JUnit 5 Tests

---
+++ src/test/java/net/damarie/JUnit5FauxTest.java
@@ -0,0 +1,10 @@
+package net.damarie;
+
+import org.junit.jupiter.api.Test;
+
+public class JUnit5FauxTest {
+    @Test
+    void someTestforJUnit5() {
+        System.out.println("I Ran...");
+    }
+}
