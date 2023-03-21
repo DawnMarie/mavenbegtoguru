4c80d293c379336cfd764e700367909da2ffe6f3
Dawn-Marie Dunn <dawn-marie.dunn@endurance.com>
2023-03-21 14:38:50 -0400

    Generating Java Classes with MapStruct and Maven

---
+++ src/main/java/net/damarie/mappers/UserMapper.java
@@ -0,0 +1,17 @@
+package net.damarie.mappers;
+
+import net.damarie.domain.User;
+import net.damarie.model.UserCommand;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserCommand userToUserCommand(User user);
+
+    User userCommandToUser(UserCommand userCommand);
+
+}
