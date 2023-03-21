4a25a965f587ed5ae9e169553b576f173fa2d5e7
Dawn-Marie Dunn <dawn-marie.dunn@endurance.com>
2023-03-21 16:38:56 -0400

    Using Maven Surefire for JUnit 5 Unit Tests

--- src/test/java/net/damarie/JavaHelloWorldTest.java
+++ src/test/java/net/damarie/JavaHelloWorldTest.java
@@ -1,7 +1,8 @@
 package net.damarie;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JavaHelloWorldTest {
 
