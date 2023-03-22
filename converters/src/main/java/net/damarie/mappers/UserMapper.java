d7c0d75989640a84acc34262a659b062126f3a25
Dawn-Marie Dunn <dawn-marie.dunn@endurance.com>
2023-03-22 15:42:33 -0400

    Add MapStruct Module

---
+++ converters/src/main/java/net/damarie/mappers/UserMapper.java
@@ -0,0 +1,17 @@
+package net.damarie.mappers;
+
+import net.damarie.entities.User;
+import net.damarie.domain.UserCommand;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserCommand userToUserCommand(User user);
+
+    User userCommandToUser(UserCommand userCommand);
+
+}
--- web-app/src/main/java/net/damarie/controllers/UserController.java
+++ web-app/src/main/java/net/damarie/controllers/UserController.java
@@ -1,11 +1,14 @@
 package net.damarie.controllers;
 
-import net.damarie.domain.UserCommand;
+import net.damarie.domain.*;
 import net.damarie.entities.User;
+import net.damarie.mappers.UserMapper;
 
 public class UserController {
 
     User saveUser(UserCommand command) {
-        return new User();
+
+        return UserMapper.INSTANCE.userCommandToUser(command);
+
     }
 }
