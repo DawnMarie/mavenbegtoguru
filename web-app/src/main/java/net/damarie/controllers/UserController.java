bda56b36b0a370fd90774ee5c3cf1867bc8a9bb4
Dawn-Marie Dunn <dawn-marie.dunn@endurance.com>
2023-03-22 14:56:17 -0400

    Maven Dependencies Between Modules

---
+++ web-app/src/main/java/net/damarie/controllers/UserController.java
@@ -0,0 +1,11 @@
+package net.damarie.controllers;
+
+import net.damarie.domain.UserCommand;
+import net.damarie.entities.User;
+
+public class UserController {
+
+    User saveUser(UserCommand command) {
+        return new User();
+    }
+}
